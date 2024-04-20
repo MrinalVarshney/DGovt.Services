5c03dbb0669eba7b190bb1dfdff2f5006ce1999a
lokesh-wagh <lokeshwagh200404@gmail.com>
2024-04-20 18:08:23 +0100

    added file upload UI

--- Hack36_1/client/src/fileUploader/fileUploadComponent.js
+++ Hack36_1/client/src/fileUploader/fileUploadComponent.js
@@ -1,21 +1,58 @@
 import React, {useEffect, useState} from "react"
 import { useWeb3 } from "../Contract_Instances/Instances"
 import addFile from "../utils/addFile";
+import readFile from "../utils/readFile";
 import { useRef } from "react";
-
 export default function Storage(){
     const [data, setData] = useState(null)
     const [fileName , setFileName] = useState(null)
     const [fileType , setFileType] = useState(null)
     const [storedFiles, setStoredFiles] = useState(null);
     const inputRef = useRef(null);
-    const { storageContract, walletAddress } = useWeb3();
+    const { storageContract, localwalletAddress}  = useWeb3();
+    const handleDownload = async(data) => {
+        const bufferData = await readFile(data.cid);
+       
+
+     
+        const blob = new Blob([bufferData], { type: 'application/octet-stream' });
+
+     
+        const url = URL.createObjectURL(blob);
+
+       
+        const downloadLink = document.createElement('a');
+
+
+        downloadLink.href = url;
+
+    
+        downloadLink.download = data.FileName + '.' + data.FileType;
+
+ 
+        document.body.appendChild(downloadLink);
+
+    
+        downloadLink.click();
+
+        
+        document.body.removeChild(downloadLink);
+     
+    }
     useEffect(() => {
-        const getFiles = async () => {
-            const res = await storageContract.methods.getFiles();
-            console.log(res);
+        if(storageContract && localwalletAddress){
+            const getFi = async () => {
+                
+                console.log(localwalletAddress)
+                const res = await storageContract.methods.getFiles(localwalletAddress).call({from : localwalletAddress});
+                // console.log(storageContract.methods.getFiles(walletAddress).call)
+                console.log(res);
+                setStoredFiles(res);
+            }
+            getFi() 
         }
-    },[])
+        
+    },[storageContract,localwalletAddress])
     const handleChange = (e) => {
         const file = e.target.files[0];
         console.log(file)
@@ -38,8 +75,9 @@ export default function Storage(){
         reader.readAsArrayBuffer(file);
     }
     const handleSubmit = async (e) => {
+        console.log(localwalletAddress)
         const cid = await addFile(data);
-        await storageContract.methods.addFile(walletAddress,fileType,fileName,cid);
+        await storageContract.methods.addFile(localwalletAddress,fileType,fileName,cid).send({from:localwalletAddress});
         console.log(cid);
     }
     return(
@@ -49,6 +87,19 @@ export default function Storage(){
             <input type="file" ref={inputRef} onChange={handleChange}></input>
             <button onClick={handleSubmit}>Submit</button>
             <br/>
+            {storedFiles?.map((data) => {
+                
+                return (
+                    <div >
+                        <button onClick={()=>{
+                            handleDownload(data);
+                        }}>
+                            {data.FileName}
+                        </button>
+                        
+                    </div>
+                )
+            })}
         </div>
     )
 }
